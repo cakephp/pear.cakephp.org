1cde37f7ce984235cff587a95607abee527e7e54
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-11-09 22:37:27 -0700

    Ensure all packages are in pirium index

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,10 @@ main() {
   # Build the pear channel from the zip files in the volume.
   pushd /data/public > /dev/null
   cp /data/pirum.xml /data/public/pirum.xml
+  rm -rf /data/public/get/*tgz
+  for i in *.tgz; do
+    ../vendor/bin/pirum add . "$i"
+  done
   ../vendor/bin/pirum build .
   popd > /dev/null
 
