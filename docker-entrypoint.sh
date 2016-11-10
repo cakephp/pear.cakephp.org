4c0c3008a72c6511354cb169b76dd53f437e015b
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2016-11-09 21:50:38 -0700

    Use an entrypoint for setting up the pirium index

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+main() {
+  # Build the pear channel from the zip files in the volume.
+  pushd /data/public > /dev/null
+  cp /data/pirum.xml /data/public/pirum.xml
+  ../vendor/bin/pirum build .
+  popd > /dev/null
+
+  nginx -g daemon off
+}
+
+main "$@"
