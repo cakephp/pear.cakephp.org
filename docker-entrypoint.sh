b5818abdafcf21feb233eed499a09a7ac40ac3c1
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-11-10 00:51:09 -0700

    remove whitespace

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,13 @@
 set -eo pipefail
 
 main() {
-  # Build the pear channel from the zip files in the volume.
   pushd /app > /dev/null
 
   mkdir -p /app/get/
-
   find /data -maxdepth 1 -type f -name "*.tar" -exec cp {} /app/get/ \;
   find /data -maxdepth 1 -type f -name "*.tgz" -exec cp {} /app/get/ \;
-
   /app/pirum build .
+
   popd > /dev/null
 
   nginx -g "daemon off;"
